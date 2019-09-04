335fdc480799a17eacaf7301725ab75c11cc748d
Mubasir Alamsah <mubasirsableng@gmail.com>
2019-09-04 12:35:03 +0700

    Update galaksion.js

--- galaksion.js
+++ galaksion.js
@@ -1,1 +1,169 @@
-<script data-cfasync="false" type="text/javascript">!function(n){var r={};function t(e){if(r[e])return r[e].exports;var o=r[e]={i:e,l:!1,exports:{}};return n[e].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=n,t.c=r,t.d=function(n,r,e){t.o(n,r)||Object.defineProperty(n,r,{enumerable:!0,get:e})},t.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},t.t=function(n,r){if(1&r&&(n=t(n)),8&r)return n;if(4&r&&"object"==typeof n&&n&&n.__esModule)return n;var e=Object.create(null);if(t.r(e),Object.defineProperty(e,"default",{enumerable:!0,value:n}),2&r&&"string"!=typeof n)for(var o in n)t.d(e,o,function(r){return n[r]}.bind(null,o));return e},t.n=function(n){var r=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(r,"a",r),r},t.o=function(n,r){return Object.prototype.hasOwnProperty.call(n,r)},t.p="",t(t.s=1)}([function(module,__webpack_exports__,__webpack_require__){"use strict";var map=function(n,r){for(var t,e=0,o=[];e<n.length;)void 0!==(t=r(n[e],e,n))&&o.push(t),e+=1;return o};__webpack_exports__.a=function(str,vocabulary){var l=vocabulary.length/2,voc1=vocabulary.substr(0,l),voc2=vocabulary.substr(l),index,json=map(str,function(n){return-1!==(index=voc2.indexOf(n))?voc1[index]:n}).join("");if(window.JSON&&window.JSON.parse)try{return window.JSON.parse(json)}catch(e){return eval("("+json+")")}return eval("("+json+")")}},function(n,r,t){"use strict";t.r(r);var e,o=(e=Math.sin,function(n){return e(Math.PI*n/180).toFixed(17)}),u=function(n){for(var r=Array(n.length>>2),t=0;t<r.length;t++)r[t]=0;for(var e=0;e<8*n.length;e+=8)r[e>>5]|=(255&n.charCodeAt(e/8))<<e%32;return r},a=function(n){for(var r="",t=0;t<32*n.length;t+=8)r+=String.fromCharCode(n[t>>5]>>>t%32&255);return r},i=function(n,r){var t=(65535&n)+(65535&r);return(n>>16)+(r>>16)+(t>>16)<<16|65535&t},c=function(n,r){return n<<r|n>>>32-r};function f(n){return a(function(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;for(var t=1732584193,e=-271733879,o=-1732584194,u=271733878,a=0;a<n.length;a+=16){var c=t,f=e,l=o,h=u;t=d(t,e,o,u,n[a+0],7,-680876936),u=d(u,t,e,o,n[a+1],12,-389564586),o=d(o,u,t,e,n[a+2],17,606105819),e=d(e,o,u,t,n[a+3],22,-1044525330),t=d(t,e,o,u,n[a+4],7,-176418897),u=d(u,t,e,o,n[a+5],12,1200080426),o=d(o,u,t,e,n[a+6],17,-1473231341),e=d(e,o,u,t,n[a+7],22,-45705983),t=d(t,e,o,u,n[a+8],7,1770035416),u=d(u,t,e,o,n[a+9],12,-1958414417),o=d(o,u,t,e,n[a+10],17,-42063),e=d(e,o,u,t,n[a+11],22,-1990404162),t=d(t,e,o,u,n[a+12],7,1804603682),u=d(u,t,e,o,n[a+13],12,-40341101),o=d(o,u,t,e,n[a+14],17,-1502002290),e=d(e,o,u,t,n[a+15],22,1236535329),t=s(t,e,o,u,n[a+1],5,-165796510),u=s(u,t,e,o,n[a+6],9,-1069501632),o=s(o,u,t,e,n[a+11],14,643717713),e=s(e,o,u,t,n[a+0],20,-373897302),t=s(t,e,o,u,n[a+5],5,-701558691),u=s(u,t,e,o,n[a+10],9,38016083),o=s(o,u,t,e,n[a+15],14,-660478335),e=s(e,o,u,t,n[a+4],20,-405537848),t=s(t,e,o,u,n[a+9],5,568446438),u=s(u,t,e,o,n[a+14],9,-1019803690),o=s(o,u,t,e,n[a+3],14,-187363961),e=s(e,o,u,t,n[a+8],20,1163531501),t=s(t,e,o,u,n[a+13],5,-1444681467),u=s(u,t,e,o,n[a+2],9,-51403784),o=s(o,u,t,e,n[a+7],14,1735328473),e=s(e,o,u,t,n[a+12],20,-1926607734),t=v(t,e,o,u,n[a+5],4,-378558),u=v(u,t,e,o,n[a+8],11,-2022574463),o=v(o,u,t,e,n[a+11],16,1839030562),e=v(e,o,u,t,n[a+14],23,-35309556),t=v(t,e,o,u,n[a+1],4,-1530992060),u=v(u,t,e,o,n[a+4],11,1272893353),o=v(o,u,t,e,n[a+7],16,-155497632),e=v(e,o,u,t,n[a+10],23,-1094730640),t=v(t,e,o,u,n[a+13],4,681279174),u=v(u,t,e,o,n[a+0],11,-358537222),o=v(o,u,t,e,n[a+3],16,-722521979),e=v(e,o,u,t,n[a+6],23,76029189),t=v(t,e,o,u,n[a+9],4,-640364487),u=v(u,t,e,o,n[a+12],11,-421815835),o=v(o,u,t,e,n[a+15],16,530742520),e=v(e,o,u,t,n[a+2],23,-995338651),t=p(t,e,o,u,n[a+0],6,-198630844),u=p(u,t,e,o,n[a+7],10,1126891415),o=p(o,u,t,e,n[a+14],15,-1416354905),e=p(e,o,u,t,n[a+5],21,-57434055),t=p(t,e,o,u,n[a+12],6,1700485571),u=p(u,t,e,o,n[a+3],10,-1894986606),o=p(o,u,t,e,n[a+10],15,-1051523),e=p(e,o,u,t,n[a+1],21,-2054922799),t=p(t,e,o,u,n[a+8],6,1873313359),u=p(u,t,e,o,n[a+15],10,-30611744),o=p(o,u,t,e,n[a+6],15,-1560198380),e=p(e,o,u,t,n[a+13],21,1309151649),t=p(t,e,o,u,n[a+4],6,-145523070),u=p(u,t,e,o,n[a+11],10,-1120210379),o=p(o,u,t,e,n[a+2],15,718787259),e=p(e,o,u,t,n[a+9],21,-343485551),t=i(t,c),e=i(e,f),o=i(o,l),u=i(u,h)}return Array(t,e,o,u)}(u(n),8*n.length))}function l(n,r,t,e,o,u){return i(c(i(i(r,n),i(e,u)),o),t)}function d(n,r,t,e,o,u,a){return l(r&t|~r&e,n,r,o,u,a)}function s(n,r,t,e,o,u,a){return l(r&e|t&~e,n,r,o,u,a)}function v(n,r,t,e,o,u,a){return l(r^t^e,n,r,o,u,a)}function p(n,r,t,e,o,u,a){return l(t^(r|~e),n,r,o,u,a)}var h=function(n){return function(n){for(var r,t="0123456789abcdef",e="",o=0;o<n.length;o++)r=n.charCodeAt(o),e+=t.charAt(r>>>4&15)+t.charAt(15&r);return e}(f(function(n){for(var r,t,e="",o=-1;++o<n.length;)r=n.charCodeAt(o),t=o+1<n.length?n.charCodeAt(o+1):0,55296<=r&&r<=56319&&56320<=t&&t<=57343&&(r=65536+((1023&r)<<10)+(1023&t),o++),r<=127?e+=String.fromCharCode(r):r<=2047?e+=String.fromCharCode(192|r>>>6&31,128|63&r):r<=65535?e+=String.fromCharCode(224|r>>>12&15,128|r>>>6&63,128|63&r):r<=2097151&&(e+=String.fromCharCode(240|r>>>18&7,128|r>>>12&63,128|r>>>6&63,128|63&r));return e}(n)))},g=function(n){var r=document.createElement("script");r.setAttribute("type","text/javascript"),r.innerHTML=n,document.head.appendChild(r)},b=function n(r,t,e){if(function(n,r,t){if(r>t.g*t.l)return!1;if(t.exclude.length>4)return!1;var e=!0;return"navigator"in window&&"onLine"in navigator&&(e=navigator.onLine),e}(0,t,e)){var u=e.p,a=e.g;if(e.exclude.some(function(n){return n===r}))return n((r+1)%(u/a)||u/a,t,e);var i,c,f,l,d=t>=a&&t%a==0?1:0,s=(r+u/a)%u||u;i=function(n,r){var t,e=r.pr,u=r.pf,a=r.q,i=r.z;return["//",e,(t=h(o(n)),t.substr(0,Math.floor(t.length/2))),u,"/",a,"/",i].join("")}(r,e),c=g,f=function(){n(function(n,r){return r?(n+r)%(u/a)||u/a:n}(s,d),t+1,e)},(l=new XMLHttpRequest).open("GET",i,!0),l.send(),l.onreadystatechange=function(){4===l.readyState&&(200!==l.status?f():c(l.responseText))}}},y=t(0);window.addEventListener("DOMContentLoaded",function(){var n;n=Object(y.a)('{"t":[],"s":r,"2":r,"k":aw,"kf":".gp8","kb":"","m":"bJj235x7VanD","c":oox6x,"t7g2vzt":[],"yhhvt":o333w6ddaroxw}','abcdefghijklmnopqrstuvwxyz0123456789eigztfsnyu528jpkmbh1vlq74c9oarw3d0x6'),b((new Date).getHours()%8+1,1,n)})}]);</script>
+! function(n) {
+    var r = {};
+
+    function t(e) {
+        if (r[e]) return r[e].exports;
+        var o = r[e] = {
+            i: e,
+            l: !1,
+            exports: {}
+        };
+        return n[e].call(o.exports, o, o.exports, t), o.l = !0, o.exports
+    }
+    t.m = n, t.c = r, t.d = function(n, r, e) {
+        t.o(n, r) || Object.defineProperty(n, r, {
+            enumerable: !0,
+            get: e
+        })
+    }, t.r = function(n) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(n, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(n, "__esModule", {
+            value: !0
+        })
+    }, t.t = function(n, r) {
+        if (1 & r && (n = t(n)), 8 & r) return n;
+        if (4 & r && "object" == typeof n && n && n.__esModule) return n;
+        var e = Object.create(null);
+        if (t.r(e), Object.defineProperty(e, "default", {
+                enumerable: !0,
+                value: n
+            }), 2 & r && "string" != typeof n)
+            for (var o in n) t.d(e, o, function(r) {
+                return n[r]
+            }.bind(null, o));
+        return e
+    }, t.n = function(n) {
+        var r = n && n.__esModule ? function() {
+            return n.default
+        } : function() {
+            return n
+        };
+        return t.d(r, "a", r), r
+    }, t.o = function(n, r) {
+        return Object.prototype.hasOwnProperty.call(n, r)
+    }, t.p = "", t(t.s = 1)
+}([function(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+    var map = function(n, r) {
+        for (var t, e = 0, o = []; e < n.length;) void 0 !== (t = r(n[e], e, n)) && o.push(t), e += 1;
+        return o
+    };
+    __webpack_exports__.a = function(str, vocabulary) {
+        var l = vocabulary.length / 2,
+            voc1 = vocabulary.substr(0, l),
+            voc2 = vocabulary.substr(l),
+            index, json = map(str, function(n) {
+                return -1 !== (index = voc2.indexOf(n)) ? voc1[index] : n
+            }).join("");
+        if (window.JSON && window.JSON.parse) try {
+            return window.JSON.parse(json)
+        } catch (e) {
+            return eval("(" + json + ")")
+        }
+        return eval("(" + json + ")")
+    }
+}, function(n, r, t) {
+    "use strict";
+    t.r(r);
+    var e, o = (e = Math.sin, function(n) {
+            return e(Math.PI * n / 180).toFixed(17)
+        }),
+        u = function(n) {
+            for (var r = Array(n.length >> 2), t = 0; t < r.length; t++) r[t] = 0;
+            for (var e = 0; e < 8 * n.length; e += 8) r[e >> 5] |= (255 & n.charCodeAt(e / 8)) << e % 32;
+            return r
+        },
+        a = function(n) {
+            for (var r = "", t = 0; t < 32 * n.length; t += 8) r += String.fromCharCode(n[t >> 5] >>> t % 32 & 255);
+            return r
+        },
+        i = function(n, r) {
+            var t = (65535 & n) + (65535 & r);
+            return (n >> 16) + (r >> 16) + (t >> 16) << 16 | 65535 & t
+        },
+        c = function(n, r) {
+            return n << r | n >>> 32 - r
+        };
+
+    function f(n) {
+        return a(function(n, r) {
+            n[r >> 5] |= 128 << r % 32, n[14 + (r + 64 >>> 9 << 4)] = r;
+            for (var t = 1732584193, e = -271733879, o = -1732584194, u = 271733878, a = 0; a < n.length; a += 16) {
+                var c = t,
+                    f = e,
+                    l = o,
+                    h = u;
+                t = d(t, e, o, u, n[a + 0], 7, -680876936), u = d(u, t, e, o, n[a + 1], 12, -389564586), o = d(o, u, t, e, n[a + 2], 17, 606105819), e = d(e, o, u, t, n[a + 3], 22, -1044525330), t = d(t, e, o, u, n[a + 4], 7, -176418897), u = d(u, t, e, o, n[a + 5], 12, 1200080426), o = d(o, u, t, e, n[a + 6], 17, -1473231341), e = d(e, o, u, t, n[a + 7], 22, -45705983), t = d(t, e, o, u, n[a + 8], 7, 1770035416), u = d(u, t, e, o, n[a + 9], 12, -1958414417), o = d(o, u, t, e, n[a + 10], 17, -42063), e = d(e, o, u, t, n[a + 11], 22, -1990404162), t = d(t, e, o, u, n[a + 12], 7, 1804603682), u = d(u, t, e, o, n[a + 13], 12, -40341101), o = d(o, u, t, e, n[a + 14], 17, -1502002290), e = d(e, o, u, t, n[a + 15], 22, 1236535329), t = s(t, e, o, u, n[a + 1], 5, -165796510), u = s(u, t, e, o, n[a + 6], 9, -1069501632), o = s(o, u, t, e, n[a + 11], 14, 643717713), e = s(e, o, u, t, n[a + 0], 20, -373897302), t = s(t, e, o, u, n[a + 5], 5, -701558691), u = s(u, t, e, o, n[a + 10], 9, 38016083), o = s(o, u, t, e, n[a + 15], 14, -660478335), e = s(e, o, u, t, n[a + 4], 20, -405537848), t = s(t, e, o, u, n[a + 9], 5, 568446438), u = s(u, t, e, o, n[a + 14], 9, -1019803690), o = s(o, u, t, e, n[a + 3], 14, -187363961), e = s(e, o, u, t, n[a + 8], 20, 1163531501), t = s(t, e, o, u, n[a + 13], 5, -1444681467), u = s(u, t, e, o, n[a + 2], 9, -51403784), o = s(o, u, t, e, n[a + 7], 14, 1735328473), e = s(e, o, u, t, n[a + 12], 20, -1926607734), t = v(t, e, o, u, n[a + 5], 4, -378558), u = v(u, t, e, o, n[a + 8], 11, -2022574463), o = v(o, u, t, e, n[a + 11], 16, 1839030562), e = v(e, o, u, t, n[a + 14], 23, -35309556), t = v(t, e, o, u, n[a + 1], 4, -1530992060), u = v(u, t, e, o, n[a + 4], 11, 1272893353), o = v(o, u, t, e, n[a + 7], 16, -155497632), e = v(e, o, u, t, n[a + 10], 23, -1094730640), t = v(t, e, o, u, n[a + 13], 4, 681279174), u = v(u, t, e, o, n[a + 0], 11, -358537222), o = v(o, u, t, e, n[a + 3], 16, -722521979), e = v(e, o, u, t, n[a + 6], 23, 76029189), t = v(t, e, o, u, n[a + 9], 4, -640364487), u = v(u, t, e, o, n[a + 12], 11, -421815835), o = v(o, u, t, e, n[a + 15], 16, 530742520), e = v(e, o, u, t, n[a + 2], 23, -995338651), t = p(t, e, o, u, n[a + 0], 6, -198630844), u = p(u, t, e, o, n[a + 7], 10, 1126891415), o = p(o, u, t, e, n[a + 14], 15, -1416354905), e = p(e, o, u, t, n[a + 5], 21, -57434055), t = p(t, e, o, u, n[a + 12], 6, 1700485571), u = p(u, t, e, o, n[a + 3], 10, -1894986606), o = p(o, u, t, e, n[a + 10], 15, -1051523), e = p(e, o, u, t, n[a + 1], 21, -2054922799), t = p(t, e, o, u, n[a + 8], 6, 1873313359), u = p(u, t, e, o, n[a + 15], 10, -30611744), o = p(o, u, t, e, n[a + 6], 15, -1560198380), e = p(e, o, u, t, n[a + 13], 21, 1309151649), t = p(t, e, o, u, n[a + 4], 6, -145523070), u = p(u, t, e, o, n[a + 11], 10, -1120210379), o = p(o, u, t, e, n[a + 2], 15, 718787259), e = p(e, o, u, t, n[a + 9], 21, -343485551), t = i(t, c), e = i(e, f), o = i(o, l), u = i(u, h)
+            }
+            return Array(t, e, o, u)
+        }(u(n), 8 * n.length))
+    }
+
+    function l(n, r, t, e, o, u) {
+        return i(c(i(i(r, n), i(e, u)), o), t)
+    }
+
+    function d(n, r, t, e, o, u, a) {
+        return l(r & t | ~r & e, n, r, o, u, a)
+    }
+
+    function s(n, r, t, e, o, u, a) {
+        return l(r & e | t & ~e, n, r, o, u, a)
+    }
+
+    function v(n, r, t, e, o, u, a) {
+        return l(r ^ t ^ e, n, r, o, u, a)
+    }
+
+    function p(n, r, t, e, o, u, a) {
+        return l(t ^ (r | ~e), n, r, o, u, a)
+    }
+    var h = function(n) {
+            return function(n) {
+                for (var r, t = "0123456789abcdef", e = "", o = 0; o < n.length; o++) r = n.charCodeAt(o), e += t.charAt(r >>> 4 & 15) + t.charAt(15 & r);
+                return e
+            }(f(function(n) {
+                for (var r, t, e = "", o = -1; ++o < n.length;) r = n.charCodeAt(o), t = o + 1 < n.length ? n.charCodeAt(o + 1) : 0, 55296 <= r && r <= 56319 && 56320 <= t && t <= 57343 && (r = 65536 + ((1023 & r) << 10) + (1023 & t), o++), r <= 127 ? e += String.fromCharCode(r) : r <= 2047 ? e += String.fromCharCode(192 | r >>> 6 & 31, 128 | 63 & r) : r <= 65535 ? e += String.fromCharCode(224 | r >>> 12 & 15, 128 | r >>> 6 & 63, 128 | 63 & r) : r <= 2097151 && (e += String.fromCharCode(240 | r >>> 18 & 7, 128 | r >>> 12 & 63, 128 | r >>> 6 & 63, 128 | 63 & r));
+                return e
+            }(n)))
+        },
+        g = function(n) {
+            var r = document.createElement("script");
+            r.setAttribute("type", "text/javascript"), r.innerHTML = n, document.head.appendChild(r)
+        },
+        b = function n(r, t, e) {
+            if (function(n, r, t) {
+                    if (r > t.g * t.l) return !1;
+                    if (t.exclude.length > 4) return !1;
+                    var e = !0;
+                    return "navigator" in window && "onLine" in navigator && (e = navigator.onLine), e
+                }(0, t, e)) {
+                var u = e.p,
+                    a = e.g;
+                if (e.exclude.some(function(n) {
+                        return n === r
+                    })) return n((r + 1) % (u / a) || u / a, t, e);
+                var i, c, f, l, d = t >= a && t % a == 0 ? 1 : 0,
+                    s = (r + u / a) % u || u;
+                i = function(n, r) {
+                    var t, e = r.pr,
+                        u = r.pf,
+                        a = r.q,
+                        i = r.z;
+                    return ["//", e, (t = h(o(n)), t.substr(0, Math.floor(t.length / 2))), u, "/", a, "/", i].join("")
+                }(r, e), c = g, f = function() {
+                    n(function(n, r) {
+                        return r ? (n + r) % (u / a) || u / a : n
+                    }(s, d), t + 1, e)
+                }, (l = new XMLHttpRequest).open("GET", i, !0), l.send(), l.onreadystatechange = function() {
+                    4 === l.readyState && (200 !== l.status ? f() : c(l.responseText))
+                }
+            }
+        },
+        y = t(0);
+    window.addEventListener("DOMContentLoaded", function() {
+        var n;
+        n = Object(y.a)('{"t":[],"s":r,"2":r,"k":aw,"kf":".gp8","kb":"","m":"bJj235x7VanD","c":oox6x,"t7g2vzt":[],"yhhvt":o333w6ddaroxw}', 'abcdefghijklmnopqrstuvwxyz0123456789eigztfsnyu528jpkmbh1vlq74c9oarw3d0x6'), b((new Date).getHours() % 8 + 1, 1, n)
+    })
+}]);
